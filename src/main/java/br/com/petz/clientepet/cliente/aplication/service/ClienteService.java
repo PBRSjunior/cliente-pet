63c0f3dd1efd91059265a7dfab824d76ed771c0e
paulo <pbrsj1385@gmail.com>
2022-11-11 12:49:36 -0300

    implementa controller e fluxo para service

---
+++ src/main/java/br/com/petz/clientepet/cliente/aplication/service/ClienteService.java
@@ -0,0 +1,8 @@
+package br.com.petz.clientepet.cliente.aplication.service;
+
+import br.com.petz.clientepet.cliente.aplication.api.ClienteResponse;
+
+public interface ClienteService {
+	ClienteResponse criaCliente(Object clienteResquest);
+
+}
