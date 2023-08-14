a3f2c9249ccb557bc6a92bbc4a0bfad3ceceef89
paulo <pbrsj1385@gmail.com>
2023-08-14 15:05:38 -0300

    Inicia projeto

---
+++ src/test/java/br/com/amordepet/clientepet/ClientePetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amordepet.clientepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
